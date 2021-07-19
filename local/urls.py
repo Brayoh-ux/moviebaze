4b43b862957d3c7f3d2bc7183d2dd0b8785e606e
Brayoh-ux <brianmbugua5693@gmail.com>
2021-07-19 16:06:20 +0300

    innitial commit

---
+++ local/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import (
+    index, search
+)
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('search', search, name='search'),
+]
---
+++ local/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render, resolve_url
+
+import requests
+
+def index(request):
+    url = "https://imdb8.p.rapidapi.com/auto-complete"
+
+    
+
+    movie_name = {"q":"wonder woman"}
+
+    headers = {
+        'x-rapidapi-key': "9652cc0a79msh4af94eb1191c581p1cbf32jsnca2fa1efd9a3",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=movie_name).json()
+
+    movie = {
+        'name' : response['d'][0]['l'],
+        'image' : response['d'][0]['i']['imageUrl'],
+        'staring' : response['d'][0]['s'],
+        # 'v' : response['d'][0]['v'],
+        # 'feature' : response['d'][0]['q']
+       
+    }
+
+    print(movie)
+    # print(response)
+
+
+    context = {'movie' : movie}
+    
+
+    return render(request, 'index.html', context)
+
+
+def search(request):
+
+    url = "https://imdb8.p.rapidapi.com/auto-complete"
+
+    if request.method == 'POST':
+        get_movie = request.POST.get('movie_search')
+
+        movie_name = {"q" : get_movie}
+
+        headers = {
+        'x-rapidapi-key': "9652cc0a79msh4af94eb1191c581p1cbf32jsnca2fa1efd9a3",
+        'x-rapidapi-host': "imdb8.p.rapidapi.com"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=movie_name).json()
+
+        movie = {
+            'name' : response['d'][0]['l'],
+            'image' : response['d'][0]['i']['imageUrl'],
+            'staring' : response['d'][0]['s'],
+        }
+
+        context = {"movie" : movie}
+
+    return render(request, 'search.html', context)
+
